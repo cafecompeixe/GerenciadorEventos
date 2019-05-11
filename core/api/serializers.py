c63b07406af19f2997659594b5c85769e108491d
Thayrone Batista <thayrone.batista@gmail.com>
2019-05-11 16:46:41 -0300

    Adicionando o sobre serializer

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework.serializers import ModelSerializer
-from core.models import Evento
+from core.models import Evento, Sobre
 
 class EventoSerializer(ModelSerializer):
     class Meta:
-        model=Evento
-        fields=(
-            "__all__"
-        )
+        model = Evento
+        fields = ('id', 'nome', 'descricao', 'data', 'atividade', 'endereco')
 
+class SobreSerializer(ModelSerializer):
+    class Meta:
+        model = Sobre
+        fields = ('__all__')
